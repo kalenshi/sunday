8a5fd3fe0e8741554a1dabc7df83333e21106515
kalenshi <kalenshi@gmail.com>
2024-04-17 09:22:49 -0300

    updated network in docker compose and redis in settings

--- sunday/settings.py
+++ sunday/settings.py
@@ -105,7 +105,7 @@ DATABASES = {
 CACHES = {
 	"default": {
 		"BACKEND": "django_redis.cache.RedisCache",
-		"LOCATION": ["redis://rdCache:6379/1", ],
+		"LOCATION": "redis://rdCache:6379/0",
 		"OPTIONS": {
 			"CLIENT_CLASS": "django_redis.client.DefaultClient",
 		},
